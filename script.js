fbcc240c5b27f969622a0a57bef5356e961d2ad9
Cris Lopes <cristinaferreira56@gmail.com>
2026-07-20 11:36:28 -0300

    Primeira versão do site Cris Lopes Art

---
+++ script.js
@@ -0,0 +1,24 @@
+const lightbox = document.getElementById("lightbox");
+const lightboxImagem = document.querySelector(".lightbox__imagem");
+const fechar = document.querySelector(".lightbox__fechar");
+
+document.querySelectorAll(".portfolio-popup").forEach(link => {
+
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        lightboxImagem.src = this.href;
+        lightbox.classList.add("ativo");
+    });
+
+});
+
+fechar.addEventListener("click", () => {
+    lightbox.classList.remove("ativo");
+});
+
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.classList.remove("ativo");
+    }
+});
